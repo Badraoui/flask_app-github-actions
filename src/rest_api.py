8f73e117a3f85413fb63264b8f178272b5ef5836
Houssem Eddine Badraoui <34254609+Badraoui@users.noreply.github.com>
2023-01-10 15:55:04 +0100

    first commit

---
+++ src/rest_api.py
@@ -0,0 +1,27 @@
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    name = os.environ.get("NAME", "World")
+    return f"Hello {name}!"
+
+
+
+@app.route("/prod")
+def hello_prod():
+    name = os.environ.get("NAME", "prod")
+    return f"voila le {name}!"
+
+@app.route("/action")
+def make_action():
+    print('it s working on console')
+
+    return "it s working on browser"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
