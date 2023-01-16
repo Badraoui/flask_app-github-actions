73c07ab2be7de6141d0ce775911a7407934c5f33
Houssem Eddine Badraoui <34254609+Badraoui@users.noreply.github.com>
2023-01-16 18:39:45 +0100

    :sparkles: deplyment

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def make_action():
 
 if app == "__main__":
 
-    app.run(debug=True,host='127.0.0.1',port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
